6340023d7a35fe324b4c882b458c103e5d4c79c8
Hamid Brz <brzhamid@gmail.com>
2025-07-06 00:29:43 -0400

    ✨ Ajout de la section À propos dans l’API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from models import db, BlogPost
+from models import db, BlogPost, Project, About
 
 app = Flask(__name__)
 CORS(app)
@@ -15,34 +15,63 @@ with app.app_context():
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    posts = BlogPost.query.all()
-    return jsonify([post.to_dict() for post in posts])
+    return jsonify([post.to_dict() for post in BlogPost.query.all()])
 
 @app.route('/api/posts', methods=['POST'])
 def create_post():
     data = request.get_json()
-    new_post = BlogPost(title=data['title'], content=data['content'])
-    db.session.add(new_post)
+    post = BlogPost(title=data['title'], content=data['content'])
+    db.session.add(post)
     db.session.commit()
-    return jsonify(new_post.to_dict()), 201
-
-@app.route('/api/posts/<int:id>', methods=['PUT'])
-def update_post(id):
-    data = request.get_json()
-    post = BlogPost.query.get_or_404(id)
-    post.title = data.get('title', post.title)
-    post.content = data.get('content', post.content)
-    db.session.commit()
-    return jsonify(post.to_dict()), 200
+    return jsonify(post.to_dict()), 201
 
 @app.route('/api/posts/<int:id>', methods=['DELETE'])
 def delete_post(id):
     post = BlogPost.query.get_or_404(id)
     db.session.delete(post)
     db.session.commit()
-    return jsonify({'message': 'Post deleted'}), 200
+    return jsonify({'message': 'deleted'}), 200
+
+@app.route('/api/projects', methods=['GET'])
+def get_projects():
+    return jsonify([p.to_dict() for p in Project.query.all()])
+
+@app.route('/api/projects', methods=['POST'])
+def create_project():
+    data = request.get_json()
+    p = Project(title=data['title'], description=data['description'], link=data['link'])
+    db.session.add(p)
+    db.session.commit()
+    return jsonify(p.to_dict()), 201
+
+@app.route('/api/projects/<int:id>', methods=['DELETE'])
+def delete_project(id):
+    p = Project.query.get_or_404(id)
+    db.session.delete(p)
+    db.session.commit()
+    return jsonify({'message': 'deleted'}), 200
+
+@app.route('/api/about', methods=['GET'])
+def get_about():
+    about = About.query.first()
+    if not about:
+        return jsonify({})
+    return jsonify(about.to_dict())
+
+@app.route('/api/about', methods=['POST'])
+def update_about():
+    data = request.get_json()
+    about = About.query.first()
+    if not about:
+        about = About(name=data['name'], title=data['title'], bio=data['bio'], image_url=data['image_url'])
+        db.session.add(about)
+    else:
+        about.name = data['name']
+        about.title = data['title']
+        about.bio = data['bio']
+        about.image_url = data['image_url']
+    db.session.commit()
+    return jsonify(about.to_dict())
 
 if __name__ == '__main__':
-    import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -4,12 +4,39 @@ db = SQLAlchemy()
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(120))
+    content = db.Column(db.Text)
+
+    def to_dict(self):
+        return {'id': self.id, 'title': self.title, 'content': self.content}
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    description = db.Column(db.Text)
+    link = db.Column(db.String(255))
 
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
-            'content': self.content
+            'description': self.description,
+            'link': self.link
+        }
+
+
+class About(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    title = db.Column(db.String(255))
+    bio = db.Column(db.Text)
+    image_url = db.Column(db.String(255))
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'title': self.title,
+            'bio': self.bio,
+            'image_url': self.image_url
         }
