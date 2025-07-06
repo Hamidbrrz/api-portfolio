4e9592cd4a3b29a91134569051d3333eb79a2398
Hamid Brz <brzhamid@gmail.com>
2025-07-06 02:24:21 -0400

    🛠️ Création automatique des tables avec db.create_all()

--- app.py
+++ app.py
@@ -108,4 +108,6 @@ def update_about():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
